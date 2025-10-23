b0274254f610f51f15e77b6775bd73e4a5fcd5b6
mariyamnavila <bibimariyamnavila@gmail.com>
2025-10-23 16:55:31 +0600

    made setup

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            lala
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomeLayout from './Layouts/HomeLayout';
+import Home from './Pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: HomeLayout,
+    children:[
+      {
+        index: true,
+        Component: Home
+      },
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
