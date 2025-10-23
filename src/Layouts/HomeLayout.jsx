0872d0a62271183a0fe56d95be77ad2e38449766
mariyamnavila <bibimariyamnavila@gmail.com>
2025-10-23 17:20:45 +0600

    added navbar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "../Components/HomeLayout/Navbar";
 const HomeLayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
